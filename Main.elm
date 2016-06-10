bef8b113320110d8909ad5bfaa848b38d5b8664c
Brendan Stromberger <brendanstromberger@gmail.com>
2016-06-09 21:09:10 -0700

    renders to shitty gl triangle

--- Main.elm
+++ Main.elm
@@ -1,26 +1,51 @@
+import Html exposing (Html)
 import Html.App as Html
-import Svg exposing (..)
-import Svg.Attributes as Attr exposing (..)
+import Html.Attributes as Attr
+import WebGL exposing (..)
 import AnimationFrame exposing (..)
 import Basics.Extra exposing (never)
+import List.Extra exposing (andThen)
 import Time
 import Window
+import Mouse
 import Task
+import Random exposing (initialSeed)
+import Dict exposing (Dict)
 import Math.Matrix4 exposing (..)
 import Math.Vector3 exposing (..)
+import Noise exposing (..)
+
+
+type alias Vertex = 
+    { position : Vec3
+    , color : Vec3
+    }
+    
 
 type alias Model = 
     { res : Maybe Window.Size
-    , tick : Int 
+    , pos : Maybe Mouse.Position
+    , terrain : Maybe (List (Vertex))
+    , tick : Int
     }
 
 
-size = 20
+type Msg
+    = Resize Window.Size
+    | MouseMove Mouse.Position
+    | Init Window.Size
+    | Tick Time.Time
+    | NoOp
+
+
+size = 16.18
 
 
 model =
     { res = Nothing
+    , pos = Nothing
     , tick = 0
+    , terrain = Nothing
     }
 
 
@@ -33,42 +58,56 @@ main =
         }
 
 
-view : Model -> Svg Msg
+mesh : Drawable Vertex
+mesh = Triangle
+  [ ( Vertex (vec3 0  0 0) (vec3 1 0 0)
+    , Vertex (vec3 1  1 0) (vec3 0 1 0)
+    , Vertex (vec3 1 -1 0) (vec3 0 0 1)
+    )
+  ]
+
+
+-- view : Model -> Svg Msg
 view ({res, tick} as model) =
     case res of
-        Nothing -> Svg.text "Nothing"
-        
-        Just {width, height} ->
-            let
-                w = (toFloat width) / size
-                h = (toFloat height) / size
-                vb = "0" ++ " " ++ "0" ++ " " ++ toString w ++ " " ++ toString h
-                
-                rows = [1..size]
-                columns = [1..size]
-                
-            in
-                Svg.svg 
-                [ viewBox vb
-                , Attr.width (toString width ++ "px")
-                , Attr.height (toString height ++ "px")
-                , Attr.style "overflow: hidden; position: absolute;" 
-                ]
-                [
-                    Svg.text' [ x (toString <| w/2), y (toString <| h/2), fontSize "1", textAnchor "middle" ] [Svg.text <| toString model] 
-                ]
-                 -- elements
+        Nothing -> Html.text "Nothing"
+        Just {height, width} ->
+            WebGL.toHtml
+                [ Attr.width width, Attr.height height ]
+                [ render vertexShader fragmentShader mesh {} ]
     
 
+init = model => Task.perform never Init Window.size
 
-init = model => Task.perform never Resize Window.size
 
-
-update : Msg -> Model -> (Model, Cmd Msg)
 update msg ({res, tick} as model) =
     case msg of
         NoOp
         -> model
+        => Cmd.none
+        
+        Init ({width, height} as res)
+        ->
+        let
+            w = width // round size
+            h = height // round size
+            (perm, newSeed) = permutationTable (initialSeed 42)
+        in 
+            { model
+            | res = Just res
+            , terrain =
+                Just 
+                [ Vertex (vec3 0  0  0) (vec3 1  0  0)
+                , Vertex (vec3 1  1  0) (vec3 0  1  0)
+                , Vertex (vec3 1 -1  0) (vec3 0  0  1)
+                ] 
+                -- Just <| [1..h] `andThen` \x -> [1..w] `andThen` \y -> 
+                --             [(x => y => Vertex (vec3 0 0 0) (vec3 0 0 0) )]
+            }
+        => Cmd.none
+        
+        MouseMove pos
+        -> { model | pos = Just pos }
         => Cmd.none
         
         Tick time
@@ -84,14 +123,33 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
         [ Window.resizes Resize
+        , Mouse.moves MouseMove
         , AnimationFrame.diffs Tick
         ]
-
-
-type Msg
-    = Resize Window.Size
-    | Tick Time.Time
-    | NoOp
     
     
+(=>) : a -> b -> (a, b)
 (=>) = (,)
+
+vertexShader : Shader { attr | position: Vec3, color: Vec3 } {} { vcolor: Vec3 }
+vertexShader = [glsl| 
+    attribute vec3 position;
+    attribute vec3 color;
+    varying vec3 vcolor;
+    
+    void main () {
+        gl_Position = vec4(position, 1.0);
+        vcolor = color;
+    }
+|]
+
+fragmentShader : Shader {} u { vcolor: Vec3 }
+fragmentShader = [glsl|
+    
+    precision mediump float;
+    varying vec3 vcolor;
+    
+    void main () {
+        gl_FragColor = vec4(vcolor, 1.0);
+    }
+|]
