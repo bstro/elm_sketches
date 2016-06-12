5ae3afccecbbf07855c4faf0026f97426c298dac
Brendan Stromberger <brendanstromberger@gmail.com>
2016-06-11 18:45:54 -0700

    triangles being drawn correctly i guess

--- Main.elm
+++ Main.elm
@@ -5,6 +5,7 @@ import WebGL exposing (..)
 import AnimationFrame exposing (..)
 import Basics.Extra exposing (never)
 import List.Extra exposing (andThen)
+import List exposing (concat)
 import Time
 import Window
 import Mouse
@@ -15,17 +16,10 @@ import Math.Matrix4 exposing (..)
 import Math.Vector3 exposing (..)
 import Noise exposing (..)
 
-
-type alias Vertex = 
-    { position : Vec3
-    , color : Vec3
-    }
-    
-
 type alias Model = 
     { res : Maybe Window.Size
     , pos : Maybe Mouse.Position
-    , terrain : Maybe (List (Vertex))
+    , terrain : Maybe (List (Vertex, Vertex, Vertex))
     , tick : Int
     }
 
@@ -38,7 +32,13 @@ type Msg
     | NoOp
 
 
-size = 16.18
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ Window.resizes Resize
+        , Mouse.moves MouseMove
+        , AnimationFrame.diffs Tick
+        ]
 
 
 model =
@@ -56,27 +56,8 @@ main =
         , init = init
         , subscriptions = subscriptions
         }
-
-
-mesh : Drawable Vertex
-mesh = Triangle
-  [ ( Vertex (vec3 0  0 0) (vec3 1 0 0)
-    , Vertex (vec3 1  1 0) (vec3 0 1 0)
-    , Vertex (vec3 1 -1 0) (vec3 0 0 1)
-    )
-  ]
-
-
--- view : Model -> Svg Msg
-view ({res, tick} as model) =
-    case res of
-        Nothing -> Html.text "Nothing"
-        Just {height, width} ->
-            WebGL.toHtml
-                [ Attr.width width, Attr.height height ]
-                [ render vertexShader fragmentShader mesh {} ]
     
-
+    
 init = model => Task.perform never Init Window.size
 
 
@@ -89,20 +70,11 @@ update msg ({res, tick} as model) =
         Init ({width, height} as res)
         ->
         let
-            w = width // round size
-            h = height // round size
             (perm, newSeed) = permutationTable (initialSeed 42)
         in 
             { model
             | res = Just res
-            , terrain =
-                Just 
-                [ Vertex (vec3 0  0  0) (vec3 1  0  0)
-                , Vertex (vec3 1  1  0) (vec3 0  1  0)
-                , Vertex (vec3 1 -1  0) (vec3 0  0  1)
-                ] 
-                -- Just <| [1..h] `andThen` \x -> [1..w] `andThen` \y -> 
-                --             [(x => y => Vertex (vec3 0 0 0) (vec3 0 0 0) )]
+            , terrain = Just <| triangle (0 , 0) (1 , 1) (1 , -1) 
             }
         => Cmd.none
         
@@ -118,38 +90,95 @@ update msg ({res, tick} as model) =
         -> { model | res = Just res }
         => Cmd.none
 
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.batch
-        [ Window.resizes Resize
-        , Mouse.moves MouseMove
-        , AnimationFrame.diffs Tick
-        ]
-    
     
 (=>) : a -> b -> (a, b)
 (=>) = (,)
 
-vertexShader : Shader { attr | position: Vec3, color: Vec3 } {} { vcolor: Vec3 }
-vertexShader = [glsl| 
-    attribute vec3 position;
+
+type alias Vertex = { position : Vec3, color : Vec3 }
+type alias Varying = { vColor : Vec3 }
+
+ 
+triangle : (Float, Float) -> (Float, Float) -> (Float, Float) -> List (Vertex, Vertex, Vertex)
+triangle (x1, y1) (x2, y2) (x3, y3) =
+    [
+        ( Vertex (vec3 x1 y1 0) (vec3 0 0 0)
+        , Vertex (vec3 x2 y2 0) (vec3 0 0 0)
+        , Vertex (vec3 x3 y3 0) (vec3 0 0 0)
+        )
+    ]
+
+
+-- n max val =
+--     let
+--         min = 2
+--     in
+--         (val - min) / (max - min) 
+
+n max unscaled =
+    let
+        maxAllowed = 1
+        minAllowed = -1
+        min = 0
+    in
+        (maxAllowed - minAllowed) * (unscaled-min) / (max-min) + minAllowed
+
+
+view ({res, tick} as model) =
+    case res of
+        Nothing ->
+            Html.text "Nothing"
+            
+        Just {height, width} ->
+            let
+                rows =
+                    40
+                columns = 
+                    40
+                pxPerRow =
+                    (toFloat height) / rows
+                pxPerCol =
+                    (toFloat width) / columns
+                getc =
+                    n columns
+                getr = 
+                    n rows
+                mesh = 
+                    Triangle <| concat <| [0..rows] `andThen` \r -> [1..columns] `andThen` \c -> [
+                        triangle
+                            (getc <| c-1  , getr <| r+1)
+                            (getc <| c    , getr <| r+1)
+                            (getc <| c-1  , getr <| r)
+                            
+                    ] 
+                log = 
+                    Debug.log "mesh is" mesh
+                    
+            in
+                WebGL.toHtml
+                    [ Attr.width width, Attr.height height ] -- Attr.style [("position", "absolute"), ("left", "-50%")] ]
+                    [ render vertexShader fragmentShader mesh {} ]
+
+
+vertexShader : Shader Vertex {} Varying
+vertexShader = [glsl|
+    attribute vec3 position; 
     attribute vec3 color;
-    varying vec3 vcolor;
+    varying vec3 vColor;
     
     void main () {
         gl_Position = vec4(position, 1.0);
-        vcolor = color;
+        vColor = color;
     }
 |]
 
-fragmentShader : Shader {} u { vcolor: Vec3 }
+fragmentShader : Shader {} {} Varying
 fragmentShader = [glsl|
-    
     precision mediump float;
-    varying vec3 vcolor;
+    
+    varying vec3 vColor;
     
     void main () {
-        gl_FragColor = vec4(vcolor, 1.0);
+        gl_FragColor = vec4(vColor, 1.0);
     }
 |]
