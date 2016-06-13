e46d3c245812df47215aad297208770881d6efb6
Brendan Stromberger <brendanstromberger@gmail.com>
2016-06-12 19:19:16 -0700

    seems to be working

--- Main.elm
+++ Main.elm
@@ -150,8 +150,8 @@ view ({res, tick, pos, texture} as model) =
                             let 
                                 uniform =
                                     { texture = tex
-                                    , rotate = rotation ((*) 0.005 <| toFloat y) ((*) 0.005 <| toFloat x)
-                                    , scaling = scaling 0.5
+                                    , rotate = rotation ((*) 0.0005 <| toFloat y) ((*) 0.0005 <| toFloat x)
+                                    , scaling = scaling 1
                                     , size = size
                                     }
                             in
@@ -182,7 +182,7 @@ type alias Uniform =
 
 
 rotation : Float -> Float -> Mat4
-rotation x y = makeRotate x (vec3 0 0 1)
+rotation x y = makeRotate x (vec3 1 0 0)
 
 scaling : Float -> Mat4
 scaling t = makeScale (vec3 t t t)  
@@ -192,7 +192,7 @@ triangle : Float -> Float -> (Float, Float) -> (Float, Float) -> (Float, Float) 
 triangle row col (x1, y1) (x2, y2) (x3, y3) =
     [
         ( Attribute (vec3 x1 y1 0) (vec3 0 1 1) row col  
-        , Attribute (vec3 x2 y2 0) (vec3 1 1 0) row col
+        , Attribute (vec3 x2 y2 0) (vec3 1 1 1) row col
         , Attribute (vec3 x3 y3 0) (vec3 1 0 1) row col
         )
     ]
@@ -212,25 +212,35 @@ vertexShader = [glsl|
     
     varying vec3 vColor;
     
-    float rand(vec2 co) {
-        return fract(sin(dot(co.xy, vec2(12.9898,78.233))) * 43758.5453);
+    highp float rand(vec2 co)
+    {
+        highp float a = 12.9898;
+        highp float b = 78.233;
+        highp float c = 43758.5453;
+        highp float dt= dot(co.xy ,vec2(a,b));
+        highp float sn= mod(dt,3.14);
+        return fract(sin(sn) * c);
     }
-            
-    void main () {        
+    
+    float n(float val) {
         float newMax = 1.0;
         float newMin = -1.0;
         float min = 0.0;
         float max = size;
         
-        float z = rand(vec2(position.x, position.y));
-        
-        vec3 newVec = vec3
-            ( (newMax - newMin) * (position.x-min) / (max-min) + newMin
-            , (newMax - newMin) * (position.y-min) / (max-min) + newMin
-            , z*10.0
+        return (newMax - newMin) * (val-min) / (max-min) + newMin;
+    }
+            
+    void main () {                        
+        vec3 newVec =
+            vec3
+            ( n(position.x)
+            , n(position.y)
+            , rand(vec2(n(position.x), n(position.y)))*4.418
+
             );
         
-        gl_Position = scaling * rotate * vec4(newVec, 1.0);
+        gl_Position = scaling * rotate * vec4(newVec, 1);
         
         vColor = color;
     }
